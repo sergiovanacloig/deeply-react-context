73d67bac1fe91252b58e0ed8da1a3742068e5e30
Sergio Vanacloig <sergiovanacloig97@gmail.com>
2022-11-30 11:49:16 +0100

    Format SearchProvider

--- src/context/Search/SearchProvider.jsx
+++ src/context/Search/SearchProvider.jsx
@@ -2,14 +2,19 @@ import { useState } from "react";
 import SearchContext from "./SearchContext";
 
 const SearchProvider = (props) => {
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const [counter, setCounter] = useState(0);
 
   const increase = () => {
     setCounter(counter + 1);
-  }
+  };
 
-  return <SearchContext.Provider value={{ search, counter, setSearch, increase }} {...props} />;
+  return (
+    <SearchContext.Provider
+      value={{ search, counter, setSearch, increase }}
+      {...props}
+    />
+  );
 };
 
 export default SearchProvider;
